35258e080b3f52d5d6bbbc483587d6b1f7ab6c37
Allen Welkie <allen@localhost.localdomain>
2015-01-10 19:01:20 -0500

    Stride struct uses slices directly

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,37 @@
-#![feature(associated_types)]
-
 extern crate num;
 
 use num::{Complex, Zero};
 use std::iter::{repeat, range_step_inclusive, range_step};
 use std::f32;
-use std::ops::{Index, IndexMut};
 use stride::{Stride, StrideMut};
 
 mod stride;
 
 pub struct FFT {
     scratch: Vec<Complex<f32>>,
-    factors: Vec<uint>,
+    factors: Vec<usize>,
 }
 
 impl FFT {
-    pub fn new(len: uint) -> Self {
+    pub fn new(len: usize) -> Self {
         FFT {
             scratch: repeat(Zero::zero()).take(len).collect(),
             factors: factor(len),
         }
     }
 
-    //pub fn process<I, IM>(&self, signal: &I, spectrum: &mut IM)
-        //where I: Index<uint, Output=Complex<f32>> + Clone,
-              //IM: IndexMut<uint, Output=Complex<f32>> + Clone {
-        //cooley_tukey(Stride::stride_trivial(signal, self.scratch.len()),
-                     //StrideMut::stride_trivial(spectrum, self.scratch.len()),
-                     //StrideMut::stride_trivial(self.scratch, self.scratch.len()),
-                     //self.factors.as_slice());
-    //}
-
+    pub fn process(&mut self, signal: &[Complex<f32>], spectrum: &mut [Complex<f32>]) {
+        cooley_tukey(Stride::from_slice(signal),
+                     StrideMut::from_slice(spectrum),
+                     StrideMut::from_slice(self.scratch.as_mut_slice()),
+                     self.factors.as_slice());
+    }
 }
 
-fn cooley_tukey<I, IM1, IM2>(signal: Stride<I>, spectrum: StrideMut<IM1>,
-                       scratch: StrideMut<IM2>, factors: &[uint])
-where I: Index<uint, Output=Complex<f32>> + Clone,
-      IM1: IndexMut<uint, Output=Complex<f32>> + Clone,
-      IM2: IndexMut<uint, Output=Complex<f32>> + Clone {
+fn cooley_tukey<'a>(signal: Stride<'a, Complex<f32>>, spectrum: StrideMut<'a, Complex<f32>>,
+                    scratch: StrideMut<'a, Complex<f32>>, factors: &[usize]) {
     match factors {
-        [1] | [] => dft(signal, spectrum),
+        [_] | [] => dft(signal, spectrum),
         [n1, other_factors..] => {
             let n2 = signal.len() / n1;
             for i in range(0, n1)
@@ -53,7 +44,7 @@ where I: Index<uint, Output=Complex<f32>> + Clone,
 
             multiply_by_twiddles(scratch.clone(), n1, n2);
 
-            for (i, offset) in range_step(0u, scratch.len(), n1).enumerate() {
+            for (i, offset) in range_step(0us, scratch.len(), n1).enumerate() {
                 let row = scratch.skip_some(offset).take_some(n1);
                 dft_mut(row, spectrum.skip_some(i));
             }
@@ -61,12 +52,12 @@ where I: Index<uint, Output=Complex<f32>> + Clone,
     }
 }
 
-fn multiply_by_twiddles<'a, I>(xs: I, n1: uint, n2: uint) where I: Iterator<Item=&'a mut Complex<f32>>
+fn multiply_by_twiddles<'a, I>(xs: I, n1: usize, n2: usize) where I: Iterator<Item=&'a mut Complex<f32>>
 {
     for (i, elt) in xs.enumerate() {
         let k2 = i / n1;
         let k1 = i % n1;
-        let angle: f32 = (-1i as f32) * ((k2 * k1) as f32) * f32::consts::PI_2 / ((n1 * n2) as f32);
+        let angle: f32 = (-1is as f32) * ((k2 * k1) as f32) * f32::consts::PI_2 / ((n1 * n2) as f32);
         let twiddle: Complex<f32> = Complex::from_polar(&1f32, &angle);
         *elt = *elt * twiddle;
     }
@@ -105,7 +96,7 @@ where I: Iterator<Item=&'a mut Complex<f32>> + ExactSizeIterator + Clone,
         let mut sum: Complex<f32> = Zero::zero();
         let mut angle = 0f32;
         let rad_per_sample = (k as f32) * f32::consts::PI_2 / (signal.len() as f32);
-        for &x in signal_iter
+        for &mut x in signal_iter
         {
             let twiddle: Complex<f32> = Complex::from_polar(&1f32, &angle);
             sum = sum + twiddle * x;
@@ -115,13 +106,13 @@ where I: Iterator<Item=&'a mut Complex<f32>> + ExactSizeIterator + Clone,
     }
 }
 
-fn factor(n: uint) -> Vec<uint>
+fn factor(n: usize) -> Vec<usize>
 {
-    let mut factors: Vec<uint> = Vec::new();
+    let mut factors: Vec<usize> = Vec::new();
     let mut next = n;
     while next > 1
     {
-        for div in Some(2u).into_iter().chain(range_step_inclusive(3u, next, 2))
+        for div in Some(2us).into_iter().chain(range_step_inclusive(3us, next, 2))
         {
             if next % div == 0
             {
--- src/stride.rs
+++ src/stride.rs
@@ -1,156 +1,163 @@
-use std::ops::{Index, IndexMut};
-use std::cmp::min;
+use std::mem;
 
 #[derive(Clone)]
-pub struct Stride<'a, I: Index<uint> + 'a> {
-    items: I,
-    base_len: uint,
-    current_idx: uint,
-    stride: uint,
+pub struct Stride<'a, A: 'a> {
+    items: &'a [A],
+    current_idx: usize,
+    stride: usize,
 }
 
-impl<'a, I: Index<uint> + Clone + 'a> Stride<'a, I> {
-    pub fn stride_trivial(xs: I, len: uint) -> Self {
+impl<'a, A> Stride<'a, A> {
+    pub fn from_slice(xs: &'a [A]) -> Self {
         Stride {
             items: xs,
-            base_len: len,
             current_idx: 0,
             stride: 1,
         }
     }
 
-    pub fn stride(&self, stride: uint) -> Stride<'a, I> {
+    pub fn stride(&self, stride: usize) -> Stride<'a, A> {
         Stride {
-            items: self.items.clone(),
-            base_len: self.base_len,
+            items: self.items,
             current_idx: self.current_idx,
             stride: self.stride * stride,
         }
     }
 
-    pub fn skip_some(&self, len: uint) -> Stride<'a, I> {
+    pub fn skip_some(&self, len: usize) -> Stride<'a, A> {
         Stride {
-            items: self.items.clone(),
-            base_len: self.base_len,
+            items: self.items,
             current_idx: self.current_idx + len * self.stride,
             stride: self.stride,
         }
     }
 
-    pub fn take_some(&self, num: uint) -> Stride<'a, I> {
+    pub fn take_some(&self, num: usize) -> Stride<'a, A> {
         Stride {
-            items: self.items.clone(),
-            base_len: min(self.base_len, num * self.stride),
+            items: self.items.slice_to((num + 1) * self.stride - 1),
             current_idx: self.current_idx,
             stride: self.stride,
         }
     }
 }
 
-impl<'a, I> Iterator for Stride<'a, I> where I: Index<uint> {
-    type Item = &'a <I as Index<uint>>::Output;
+impl<'a, A> Iterator for Stride<'a, A> where A: 'a {
+    type Item = &'a A;
 
     #[inline]
     fn next(&mut self) -> Option<<Self as Iterator>::Item> {
-        if (self.current_idx >= self.base_len) {
+        if self.current_idx >= self.items.len() {
             None
         } else {
             let idx = self.current_idx;
             self.current_idx += self.stride;
-            Some(self.items.index(&idx))
+            unsafe {
+                Some(self.items.get_unchecked(idx))
+            }
         }
     }
 
     #[inline]
-    fn size_hint(&self) -> (uint, Option<uint>) {
-        let len = (self.base_len - self.current_idx) / self.stride + 
-            if (self.base_len - self.current_idx) % self.stride > 0 { 1 } else { 0 };
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = (self.items.len() - self.current_idx) / self.stride + 
+            if (self.items.len() - self.current_idx) % self.stride > 0 { 1 } else { 0 };
         (len, Some(len))
     }
 }
 
-impl<'a, I> DoubleEndedIterator for Stride<'a, I> where I: Index<uint> {
-    fn next_back(&mut self) -> Option<&'a <I as Index<uint>>::Output> {
+impl<'a, A> DoubleEndedIterator for Stride<'a, A> where A: 'a {
+    fn next_back(&mut self) -> Option<&'a A> {
         panic!("Not implemented");
         None
     }
 }
 
-impl<'a, I> ExactSizeIterator for Stride<'a, I> where I: Index<uint> { }
+impl<'a, A> ExactSizeIterator for Stride<'a, A> where A: 'a { }
 
-#[derive(Clone)]
-pub struct StrideMut<'a, I: IndexMut<uint> + 'a> {
-    items: I,
-    base_len: uint,
-    current_idx: uint,
-    stride: uint,
+pub struct StrideMut<'a, A: 'a> {
+    items: &'a mut [A],
+    current_idx: usize,
+    stride: usize,
 }
 
-impl<'a, I: IndexMut<uint> + Clone + 'a> StrideMut<'a, I> {
-    pub fn stride_trivial(xs: I, len: uint) -> Self {
+impl<'a, A> Clone for StrideMut<'a, A> {
+    fn clone(&self) -> Self {
+        let items_copy = unsafe{ mem::transmute_copy(&self.items) };
+        StrideMut {
+            items: items_copy,
+            current_idx: self.current_idx,
+            stride: self.stride,
+        }
+    }
+}
+
+impl<'a, A> StrideMut<'a, A> {
+    pub fn from_slice(xs: &'a mut [A]) -> Self {
         StrideMut {
             items: xs,
-            base_len: len,
             current_idx: 0,
             stride: 1,
         }
     }
 
-    pub fn stride(&self, stride: uint) -> StrideMut<'a, I> {
+    pub fn stride(&self, stride: usize) -> StrideMut<'a, A> {
+        let items_copy = unsafe{ mem::transmute_copy(&self.items) };
         StrideMut {
-            items: self.items.clone(),
-            base_len: self.base_len,
+            items: items_copy,
             current_idx: self.current_idx,
             stride: self.stride * stride,
         }
     }
 
-    pub fn skip_some(&self, len: uint) -> StrideMut<'a, I> {
+    pub fn skip_some(&self, len: usize) -> StrideMut<'a, A> {
+        let items_copy = unsafe{ mem::transmute_copy(&self.items) };
         StrideMut {
-            items: self.items.clone(),
-            base_len: self.base_len,
+            items: items_copy,
             current_idx: self.current_idx + len * self.stride,
             stride: self.stride,
         }
     }
 
-    pub fn take_some(&self, num: uint) -> StrideMut<'a, I> {
+    pub fn take_some(&self, num: usize) -> StrideMut<'a, A> {
+        let items_copy: &'a mut [A] = unsafe{ mem::transmute_copy(&self.items) };
         StrideMut {
-            items: self.items.clone(),
-            base_len: min(self.base_len, num * self.stride),
+            items: items_copy.slice_to_mut((num + 1) * self.stride - 1),
             current_idx: self.current_idx,
             stride: self.stride,
         }
     }
 }
 
-impl<'a, I> Iterator for StrideMut<'a, I> where I: IndexMut<uint> {
-    type Item = &'a mut <I as IndexMut<uint>>::Output;
+impl<'a, A> Iterator for StrideMut<'a, A> where A: 'a {
+    type Item = &'a mut A;
 
     #[inline]
     fn next(&mut self) -> Option<<Self as Iterator>::Item> {
-        if self.current_idx >= self.base_len {
+        if self.current_idx >= self.items.len() {
             None
         } else {
             let idx = self.current_idx;
             self.current_idx += self.stride;
-            Some(self.items.index_mut(&idx))
+            unsafe {
+                let elt_ref = mem::transmute(self.items.get_unchecked_mut(idx));
+                Some(elt_ref)
+            }
         }
     }
 
     #[inline]
-    fn size_hint(&self) -> (uint, Option<uint>) {
-        let len = (self.base_len - self.current_idx) / self.stride + 
-            if (self.base_len - self.current_idx) % self.stride > 0 { 1 } else { 0 };
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = (self.items.len() - self.current_idx) / self.stride + 
+            if (self.items.len() - self.current_idx) % self.stride > 0 { 1 } else { 0 };
         (len, Some(len))
     }
 }
 
-impl<'a, I> DoubleEndedIterator for StrideMut<'a, I> where I: IndexMut<uint> {
-    fn next_back(&mut self) -> Option<&'a mut <I as IndexMut<uint>>::Output> {
+impl<'a, A> DoubleEndedIterator for StrideMut<'a, A> where A: 'a {
+    fn next_back(&mut self) -> Option<&'a mut A> {
         panic!("Not implemented");
         None
     }
 }
 
-impl<'a, I> ExactSizeIterator for StrideMut<'a, I> where I: IndexMut<uint> { }
+impl<'a, A> ExactSizeIterator for StrideMut<'a, A> where A: 'a { }
