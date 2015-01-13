42e61c9ccba576602fedbdcdb2ec19d2f591da2a
Allen Welkie <allen@localhost.localdomain>
2015-01-12 20:14:37 -0500

    Added more benches

--- benches/kissfft.rs
+++ benches/kissfft.rs
@@ -2,12 +2,24 @@ extern crate test;
 extern crate kissfft;
 
 use test::Bencher;
+use std::iter::repeat;
+use kissfft::Complex;
+
+/// Times just the FFT execution (not allocation and pre-calculation)
+/// for a given length
+fn bench_fft(b: &mut Bencher, len: usize) {
+    let mut kiss_fft = kissfft::KissFFT::new(len, false);
+    let signal: Vec<Complex> = repeat(Complex{r:0.,i:0.}).take(len).collect();
+    let mut spectrum: Vec<Complex> = repeat(Complex{r:0.,i:0.}).take(len).collect();
+    b.iter(|&mut:| {kiss_fft.transform(signal.as_slice(), spectrum.as_mut_slice());} );
+}
+
+#[bench]
+fn kiss_fft_2401_fft(b: &mut Bencher) {
+    bench_fft(b, 2401);
+}
 
 #[bench]
 fn kiss_fft_343_fft(b: &mut Bencher) {
-    const len: usize = 343;
-    let mut kiss_fft = kissfft::KissFFT::new(len, false);
-    let fin = [kissfft::Complex { r: 0.0, i: 0.0 }; len];
-    let mut fout = [kissfft::Complex { r: 0.0, i: 0.0 }; len];
-    b.iter(|&mut:| {kiss_fft.transform(&fin, &mut fout);} );
+    bench_fft(b, 343);
 }
--- benches/rustfft.rs
+++ benches/rustfft.rs
@@ -4,12 +4,23 @@ extern crate rustfft;
 
 use test::Bencher;
 use num::Complex;
+use std::iter::repeat;
+
+/// Times just the FFT execution (not allocation and pre-calculation)
+/// for a given length
+fn bench_fft(b: &mut Bencher, len: usize) {
+    let mut fft = rustfft::FFT::new(len);
+    let signal: Vec<Complex<f32>> = repeat(Complex{re:0.,im:0.}).take(len).collect();
+    let mut spectrum: Vec<Complex<f32>> = repeat(Complex{re:0.,im:0.}).take(len).collect();
+    b.iter(|&mut:| {fft.process(signal.as_slice(), spectrum.as_mut_slice());} );
+}
+
+#[bench]
+fn rust_fft_2401_fft(b: &mut Bencher) {
+    bench_fft(b, 2401);
+}
 
 #[bench]
 fn rust_fft_343_fft(b: &mut Bencher) {
-    const len: usize = 343;
-    let mut fft = rustfft::FFT::new(len);
-    let fin = [Complex { re: 0.0, im: 0.0 }; len];
-    let mut fout = [Complex { re: 0.0, im: 0.0 }; len];
-    b.iter(|&mut:| {fft.process(&fin, &mut fout);} );
+    bench_fft(b, 343);
 }
