e58b5db31a32cff03c0caba757e4d3ea2c6fe698
Allen Welkie <allen@localhost.localdomain>
2015-01-14 19:46:36 -0500

    Compile without abstractions. Tests fail though

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,11 @@ extern crate num;
 use num::{Complex, Zero};
 use std::iter::{repeat, range_step_inclusive, range_step};
 use std::f32;
-use stride::{Stride, StrideMut};
-
-mod stride;
 
 pub struct FFT {
     scratch: Vec<Complex<f32>>,
     factors: Vec<usize>,
-    twiddles: TwiddlePowerTable,
+    twiddles: Vec<Complex<f32>>,
 }
 
 impl FFT {
@@ -18,74 +15,77 @@ impl FFT {
         FFT {
             scratch: repeat(Zero::zero()).take(len).collect(),
             factors: factor(len),
-            twiddles: TwiddlePowerTable::new(len),
+            twiddles: range(0, len)
+                      .map(|i| -1. * (i as f32) * f32::consts::PI_2 / (len as f32))
+                      .map(|phase| Complex::from_polar(&1., &phase))
+                      .collect(),
         }
     }
 
     pub fn process(&mut self, signal: &[Complex<f32>], spectrum: &mut [Complex<f32>]) {
-        cooley_tukey(Stride::from_slice(signal),
-                     StrideMut::from_slice(spectrum),
-                     StrideMut::from_slice(self.scratch.as_mut_slice()),
-                     &self.twiddles,
+        cooley_tukey(signal, 1,
+                     spectrum, 1,
+                     self.scratch.as_mut_slice(), 1,
+                     self.twiddles.as_slice(),
                      self.factors.as_slice());
     }
 }
 
-fn cooley_tukey<'a>(signal: Stride<'a, Complex<f32>>,
-                    spectrum: StrideMut<'a, Complex<f32>>,
-                    scratch: StrideMut<'a, Complex<f32>>,
-                    twiddles: &TwiddlePowerTable,
-                    factors: &[usize]) {
+fn cooley_tukey(signal: &[Complex<f32>],
+                signal_stride: usize,
+                spectrum: &mut [Complex<f32>],
+                spectrum_stride: usize,
+                scratch: &mut [Complex<f32>],
+                scratch_stride: usize,
+                twiddles: &[Complex<f32>],
+                factors: &[usize]) {
     match factors {
-        [_] | [] => dft_precalc_twiddles(signal, spectrum, twiddles),
+        [] => {
+            for i in range_step(0, signal.len(), signal_stride) {
+                unsafe { *scratch.get_unchecked_mut(i) = *signal.get_unchecked(i) };
+            }
+        },
         [n1, other_factors..] => {
             let n2 = signal.len() / n1;
             for i in range(0, n1)
             {
-                cooley_tukey(signal.skip_some(i).stride(n1),
-                             scratch.skip_some(i).stride(n1),
-                             spectrum.skip_some(i).stride(n1),
-                             twiddles,
-                             other_factors);
+                cooley_tukey(signal.slice_from(i), signal_stride * n1,
+                             scratch.slice_from_mut(i), scratch_stride * n1,
+                             spectrum.slice_from_mut(i), spectrum_stride * n1,
+                             twiddles, other_factors);
             }
 
-            butterfly(scratch, spectrum, twiddles, n1, n2);
+            butterfly(scratch, scratch_stride,
+                      spectrum, spectrum_stride,
+                      twiddles, n1, n2);
         }
     }
 }
 
-fn butterfly<'a>(input: StrideMut<'a, Complex<f32>>,
-                 output: StrideMut<'a, Complex<f32>>,
-                 twiddles: &TwiddlePowerTable,
-                 num_cols: usize,
-                 num_rows: usize) {
-    for (i, offset) in range_step(0us, input.len(), num_cols).enumerate() {
-        let in_row = input.skip_some(offset).take_some(num_cols);
-        let out_col = output.skip_some(i).stride(num_rows);
-        for (j, spec_bin) in out_col.enumerate() {
+fn butterfly(input: &[Complex<f32>],
+             input_stride: usize,
+             output: &mut [Complex<f32>],
+             output_stride: usize,
+             twiddles: &[Complex<f32>],
+             num_cols: usize,
+             num_rows: usize) {
+    // for each row in input
+    for (i, in_row) in input.chunks(num_cols * input_stride).enumerate() {
+        let out_col = output.slice_from_mut(i);
+        let out_col_stride = output_stride * num_rows;
+        for (j, spec_bin_idx) in range_step(0, out_col.len(), out_col_stride).enumerate() {
+            let spec_bin = unsafe { out_col.get_unchecked_mut(spec_bin_idx) };
             *spec_bin = Zero::zero();
-            let in_row_copy = in_row.clone();
-            for (k, sig_bin) in in_row_copy.enumerate() {
+            for (k, sig_bin_idx) in range_step(0, in_row.len(), input_stride).enumerate() {
+                let sig_bin = unsafe { input.get_unchecked(sig_bin_idx) };
                 let twiddle_power = i * k + j * k * num_rows;
-                let twiddle = twiddles.get_twiddle(twiddle_power, num_rows * num_cols);
-                *spec_bin = *spec_bin + twiddle * *sig_bin;
+                let n = twiddles.len();
+                let twiddle = unsafe {
+                    twiddles.get_unchecked((twiddle_power * n ) /
+                                           (num_cols * num_rows) % n)
+                };
+                *spec_bin = *spec_bin + *twiddle * *sig_bin;
             }
-        }
-    }
-}
-
-fn dft_precalc_twiddles<'a, 'b, I, O>(signal: I, spectrum: O, twiddles: &TwiddlePowerTable)
-where I: Iterator<Item=&'a Complex<f32>> + ExactSizeIterator + Clone,
-      O: Iterator<Item=&'b mut Complex<f32>>
-{
-    let n = signal.len();
-    for (k, spec_bin) in spectrum.enumerate()
-    {
-        let mut signal_iter = signal.clone();
-        *spec_bin = Zero::zero();
-        for (i, &x) in signal_iter.enumerate() {
-            let twiddle = twiddles.get_twiddle(i * k, n);
-            *spec_bin = *spec_bin + twiddle * x;
         }
     }
 }
@@ -124,33 +124,4 @@ fn factor(n: usize) -> Vec<usize>
         }
     }
     return factors;
-}
-
-/// Holds the powers of the `n`th root of unity
-struct TwiddlePowerTable {
-    n: usize,
-    twiddles: Vec<Complex<f32>>,
-}
-
-impl TwiddlePowerTable {
-    /// Calculates the powers of the `n`th root of unity
-    fn new(n: usize) -> Self {
-        TwiddlePowerTable {
-            n: n,
-            twiddles: range(0, n)
-                      .map(|i| -1. * (i as f32) * f32::consts::PI_2 / (n as f32))
-                      .map(|phase| Complex::from_polar(&1., &phase))
-                      .collect(),
-        }
-    }
-
-    /// Returns the `base`th root of unity raised to `power`.
-    ///
-    /// `base` must be a divisor of the value of `n` for which the table was calculated.
-    #[inline]
-    fn get_twiddle(&self, power: usize, base: usize) -> &Complex<f32> {
-        unsafe {
-            self.twiddles.get_unchecked((power * self.n / base) % self.n)
-        }
-    }
 }
