dc7ff43b46b72695565d9a130fa3a2caf68c8113
Allen Welkie <allen@localhost.localdomain>
2015-01-14 22:26:16 -0500

    Fixed bugs in no abstraction calculation. Tests now pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(unstable)]
 extern crate num;
 
 use num::{Complex, Zero};
@@ -31,6 +32,7 @@ impl FFT {
     }
 }
 
+//TODO can we collapse all these strides into one stride value?
 fn cooley_tukey(signal: &[Complex<f32>],
                 signal_stride: usize,
                 spectrum: &mut [Complex<f32>],
@@ -40,18 +42,20 @@ fn cooley_tukey(signal: &[Complex<f32>],
                 twiddles: &[Complex<f32>],
                 factors: &[usize]) {
     match factors {
-        [] => {
-            for i in range_step(0, signal.len(), signal_stride) {
-                unsafe { *scratch.get_unchecked_mut(i) = *signal.get_unchecked(i) };
-            }
-        },
+        [_] | [] => dft_slice(signal, signal_stride,
+                              spectrum, spectrum_stride,
+                              twiddles),
         [n1, other_factors..] => {
-            let n2 = signal.len() / n1;
+            //SPEED precalculate n1 AND n2
+            let n2 = signal.len() / (n1 * signal_stride) +
+                if signal.len() % (n1 * signal_stride) > 0 {1} else {0};
             for i in range(0, n1)
             {
-                cooley_tukey(signal.slice_from(i), signal_stride * n1,
-                             scratch.slice_from_mut(i), scratch_stride * n1,
-                             spectrum.slice_from_mut(i), spectrum_stride * n1,
+                // perform the smaller FFTs from the signal buffer into
+                // the scratch buffer, using the spectrum buffer as scratch space
+                cooley_tukey(signal.slice_from(i * signal_stride), signal_stride * n1,
+                             scratch.slice_from_mut(i * scratch_stride), scratch_stride * n1,
+                             spectrum.slice_from_mut(i * spectrum_stride), spectrum_stride * n1,
                              twiddles, other_factors);
             }
 
@@ -71,21 +75,39 @@ fn butterfly(input: &[Complex<f32>],
              num_rows: usize) {
     // for each row in input
     for (i, in_row) in input.chunks(num_cols * input_stride).enumerate() {
-        let out_col = output.slice_from_mut(i);
+        let out_col = output.slice_from_mut(i * output_stride);
         let out_col_stride = output_stride * num_rows;
         for (j, spec_bin_idx) in range_step(0, out_col.len(), out_col_stride).enumerate() {
             let spec_bin = unsafe { out_col.get_unchecked_mut(spec_bin_idx) };
             *spec_bin = Zero::zero();
             for (k, sig_bin_idx) in range_step(0, in_row.len(), input_stride).enumerate() {
-                let sig_bin = unsafe { input.get_unchecked(sig_bin_idx) };
+                let sig_bin = unsafe { in_row.get_unchecked(sig_bin_idx) };
                 let twiddle_power = i * k + j * k * num_rows;
                 let n = twiddles.len();
                 let twiddle = unsafe {
-                    twiddles.get_unchecked((twiddle_power * n ) /
-                                           (num_cols * num_rows) % n)
+                    twiddles.get_unchecked(((twiddle_power * n ) /
+                                           (num_cols * num_rows)) % n)
                 };
                 *spec_bin = *spec_bin + *twiddle * *sig_bin;
             }
+        }
+    }
+}
+
+pub fn dft_slice(signal: &[Complex<f32>],
+                 signal_stride: usize,
+                 spectrum: &mut [Complex<f32>],
+                 spectrum_stride: usize,
+                 twiddles: &[Complex<f32>]) {
+    for (k, spec_bin_idx) in range_step(0, spectrum.len(), spectrum_stride).enumerate() {
+        let spec_bin = unsafe { spectrum.get_unchecked_mut(spec_bin_idx) };
+        *spec_bin = Zero::zero();
+        for (i, signal_bin_idx) in range_step(0, signal.len(), signal_stride).enumerate() {
+            let signal_bin = unsafe { signal.get_unchecked(signal_bin_idx) };
+            let twiddle = unsafe {
+                //SPEED we can add and subtract by twiddles.len, instead of modulo
+                twiddles.get_unchecked((i * k * signal_stride) % twiddles.len()) };
+            *spec_bin = *spec_bin + twiddle * *signal_bin;
         }
     }
 }
@@ -96,7 +118,7 @@ where I: Iterator<Item=&'a Complex<f32>> + ExactSizeIterator + Clone,
 {
     for (k, spec_bin) in spectrum.enumerate()
     {
-        let mut signal_iter = signal.clone();
+        let signal_iter = signal.clone();
         let mut sum: Complex<f32> = Zero::zero();
         for (i, &x) in signal_iter.enumerate() {
             let angle = -1. * ((i * k) as f32) * f32::consts::PI_2 / (signal.len() as f32);
