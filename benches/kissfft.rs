09a4e26aa6816fac2cec5c18bd46b68af82cb366
Allen Welkie <allen@localhost.localdomain>
2015-02-18 22:35:30 -0500

    Added rust-kissfft submodule and kissfft butterfly2 benchmark

--- benches/kissfft.rs
+++ benches/kissfft.rs
@@ -1,5 +1,6 @@
 extern crate test;
 extern crate kissfft;
+extern crate libc;
 
 use test::Bencher;
 use std::iter::repeat;
@@ -37,3 +38,22 @@ fn kiss_fft_2pow14_fft(b: &mut Bencher) { bench_fft(b, 16384); }
 fn kiss_fft_goodmix222_fft(b: &mut Bencher) { bench_fft(b, 900); }
 #[bench]
 fn kiss_fft_goodmix332_fft(b: &mut Bencher) { bench_fft(b, 5400); }
+
+#[bench]
+fn kiss_fft_butterfly_2(b: &mut Bencher) {
+    let stride = 4us;
+    let num_ffts = 1000us;
+    let len = 2 * stride * num_ffts;
+
+    let cfg = unsafe {
+        kissfft::binding::kiss_fft_alloc(len as libc::c_int, 0 as libc::c_int, std::ptr::null_mut(), std::ptr::null_mut())
+    };
+
+    let mut data: Vec<Complex> = repeat(Complex{r:0.,i:0.}).take(len).collect();
+
+    b.iter(||
+    unsafe {
+        kissfft::binding::kf_bfly2(data.as_mut_ptr(), stride as libc::size_t, cfg, num_ffts as libc::c_int)
+    }
+    );
+}
