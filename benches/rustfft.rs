fb796b667db416f959908f770b22b930bac98e66
Allen Welkie <allen.welkie@gmail.com>
2015-12-16 16:12:55 -0500

    Cleaned up benchmarks
    Removed KissFFT benchmarks
    Changed the names of the benchmarks for prettier printing

--- benches/rustfft.rs
+++ benches/rustfft.rs
@@ -5,37 +5,32 @@ extern crate rustfft;
 
 use test::Bencher;
 use num::Complex;
-use std::iter::repeat;
 
 /// Times just the FFT execution (not allocation and pre-calculation)
 /// for a given length
 fn bench_fft(b: &mut Bencher, len: usize) {
     let mut fft = rustfft::FFT::new(len, false);
-    let signal: Vec<Complex<f32>> = repeat(Complex{re:0.,im:0.}).take(len).collect();
-    let mut spectrum: Vec<Complex<f32>> = repeat(Complex{re:0.,im:0.}).take(len).collect();
+    let signal = vec![Complex{re: 0.0, im: 0.0}; len];
+    let mut spectrum = signal.clone();
     b.iter(|| {fft.process(&signal[..], &mut spectrum[..]);} );
 }
 
-// Powers of 7
-#[bench]
-fn rust_fft_7pow3_fft(b: &mut Bencher) { bench_fft(b, 343); }
-#[bench]
-fn rust_fft_7pow4_fft(b: &mut Bencher) { bench_fft(b, 2401); }
-#[bench]
-fn rust_fft_7pow5_fft(b: &mut Bencher) { bench_fft(b, 16807); }
-
 // Powers of 2
-#[bench]
-fn rust_fft_2pow8_fft(b: &mut Bencher) { bench_fft(b, 256); }
-#[bench]
-fn rust_fft_2pow10_fft(b: &mut Bencher) { bench_fft(b, 1024); }
-#[bench]
-fn rust_fft_2pow12_fft(b: &mut Bencher) { bench_fft(b, 4096); }
-#[bench]
-fn rust_fft_2pow14_fft(b: &mut Bencher) { bench_fft(b, 16384); }
+#[bench] fn complex_p2_00064(b: &mut Bencher) { bench_fft(b,    64); }
+#[bench] fn complex_p2_00256(b: &mut Bencher) { bench_fft(b,   256); }
+#[bench] fn complex_p2_01024(b: &mut Bencher) { bench_fft(b,  1024); }
+#[bench] fn complex_p2_04096(b: &mut Bencher) { bench_fft(b,  4096); }
+#[bench] fn complex_p2_08192(b: &mut Bencher) { bench_fft(b,  8192); }
+#[bench] fn complex_p2_16384(b: &mut Bencher) { bench_fft(b, 16384); }
 
-// Mixed powers of 2, 3, and 5
-#[bench]
-fn rust_fft_goodmix222_fft(b: &mut Bencher) { bench_fft(b, 900); }
-#[bench]
-fn rust_fft_goodmix332_fft(b: &mut Bencher) { bench_fft(b, 5400); }
+// Powers of 7
+#[bench] fn complex_p7_00343(b: &mut Bencher) { bench_fft(b,   343); }
+#[bench] fn complex_p7_02401(b: &mut Bencher) { bench_fft(b,  2401); }
+#[bench] fn complex_p7_16807(b: &mut Bencher) { bench_fft(b, 16807); }
+
+// some other random lengths
+#[bench] fn complex_0100(b: &mut Bencher) { bench_fft(b,  100); }
+#[bench] fn complex_0900(b: &mut Bencher) { bench_fft(b,  900); }
+#[bench] fn complex_1000(b: &mut Bencher) { bench_fft(b, 1000); }
+#[bench] fn complex_1009(b: &mut Bencher) { bench_fft(b, 1009); }
+#[bench] fn complex_1260(b: &mut Bencher) { bench_fft(b, 1260); }
