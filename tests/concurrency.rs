3a76b16e293f244a6254751a195c0d57151b3891
Allen Welkie <allen.welkie@gmail.com>
2017-05-19 13:05:32 -0400

    Add test for concurrently using an FFT object

---
+++ tests/concurrency.rs
@@ -0,0 +1,28 @@
+//! Test that we can use an `FFT` object from multiple threads
+
+extern crate rustfft;
+
+use std::thread;
+
+use rustfft::FFTplanner;
+use rustfft::num_complex::Complex32;
+
+#[test]
+fn two_threads() {
+    let inverse = false;
+    let mut planner = FFTplanner::new(inverse);
+    let fft = planner.plan_fft(100);
+
+    let threads: Vec<thread::JoinHandle<_>> = (0..2).map(|_| {
+        let fft_copy = fft.clone();
+        thread::spawn(move || {
+            let mut signal = vec![Complex32::new(0.0, 0.0); 100];
+            let mut spectrum = vec![Complex32::new(0.0, 0.0); 100];
+            fft_copy.process(&mut signal, &mut spectrum);
+        })
+    }).collect();
+
+    for thread in threads {
+        thread.join().unwrap();
+    }
+}
