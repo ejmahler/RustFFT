4129f7109bf78c44b5fc90905d3a2a8e8059cc26
justwalt <36315275+justwalt@users.noreply.github.com>
2019-11-26 21:48:16 -0700

    Fixed spelling (#47)

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,6 @@
 use num_traits::{FromPrimitive, Signed};
 
-/// Generic floating point number, implemnted for f32 and f64
+/// Generic floating point number, implemented for f32 and f64
 pub trait FFTnum: Copy + FromPrimitive + Signed + Sync + Send + 'static {}
 
 impl FFTnum for f32 {}
