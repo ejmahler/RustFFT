696b43e049f5a99e6e0b36fa9b5900032680b188
Gernot Bauer <bauer@itt.uni-stuttgart.de>
2020-11-26 13:35:49 +0100

    Changed trait bounds from Signed to Num + Neg. Added generic impl

--- src/common.rs
+++ src/common.rs
@@ -1,21 +1,46 @@
-use num_traits::{FromPrimitive, Signed};
+use num_traits::{FromPrimitive, Num};
+use std::ops::Neg;
 
 /// Generic floating point number, implemented for f32 and f64
-pub trait FFTnum: Copy + FromPrimitive + Signed + Sync + Send + 'static {}
+pub trait FFTnum: Copy + FromPrimitive + Num + Neg<Output = Self> + Sync + Send + 'static {}
 
-impl FFTnum for f32 {}
-impl FFTnum for f64 {}
-
+// impl FFTnum for f32 {}
+// impl FFTnum for f64 {}
+impl<T> FFTnum for T where T: Copy + FromPrimitive + Num + Neg<Output = Self> + Sync + Send + 'static
+{}
 
 #[inline(always)]
 pub fn verify_length<T>(input: &[T], output: &[T], expected: usize) {
-	assert_eq!(input.len(), expected, "Input is the wrong length. Expected {}, got {}", expected, input.len());
-	assert_eq!(output.len(), expected, "Output is the wrong length. Expected {}, got {}", expected, output.len());
+    assert_eq!(
+        input.len(),
+        expected,
+        "Input is the wrong length. Expected {}, got {}",
+        expected,
+        input.len()
+    );
+    assert_eq!(
+        output.len(),
+        expected,
+        "Output is the wrong length. Expected {}, got {}",
+        expected,
+        output.len()
+    );
 }
-
 
 #[inline(always)]
 pub fn verify_length_divisible<T>(input: &[T], output: &[T], expected: usize) {
-	assert_eq!(input.len() % expected, 0, "Input is the wrong length. Expected multiple of {}, got {}", expected, input.len());
-	assert_eq!(input.len(), output.len(), "Input and output must have the same length. Expected {}, got {}", input.len(), output.len());
+    assert_eq!(
+        input.len() % expected,
+        0,
+        "Input is the wrong length. Expected multiple of {}, got {}",
+        expected,
+        input.len()
+    );
+    assert_eq!(
+        input.len(),
+        output.len(),
+        "Input and output must have the same length. Expected {}, got {}",
+        input.len(),
+        output.len()
+    );
 }
