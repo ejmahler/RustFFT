6fbb3f41feab9f0e60feb6455fe1b46095a19106
Elliott Mahler <join.together@gmail.com>
2020-12-18 07:53:44 -0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -3,7 +3,8 @@ use num_traits::{FromPrimitive, Signed};
 /// Generic floating point number
 pub trait FFTnum: Copy + FromPrimitive + Signed + Sync + Send + 'static {}
 
-// Blanket impl for any type that implements the required traits
+// Blanket impl for any type that implements the required traits. 
+// Notably, this includes f32 and f64, but also allows flexibility for library authors of custom numeric types.
 impl<T> FFTnum for T where T: Copy + FromPrimitive + Signed + Sync + Send + 'static {}
 
 
