9b97be687b76c61f43608f22deb73fb2bd9b6906
Henrik Enquist <henrik.enquist@gmail.com>
2021-09-23 14:56:56 +0200

    Add more benches

--- src/neon/neon_butterflies.rs
+++ src/neon/neon_butterflies.rs
@@ -2782,8 +2782,8 @@ boilerplate_fft_neon_f32_butterfly!(NeonF32Butterfly32, 32, |this: &NeonF32Butte
 boilerplate_fft_neon_common_butterfly!(NeonF32Butterfly32, 32, |this: &NeonF32Butterfly32<_>| this
     .direction);
 impl<T: FftNum> NeonF32Butterfly32<T> {
-    #[inline(always)]
-    pub fn new(direction: FftDirection) -> Self {
+    #[target_feature(enable = "neon")]
+    unsafe fn neon_new(direction: FftDirection) -> Self {
         assert_f32::<T>();
         let bf8 = NeonF32Butterfly8::new(direction);
         let bf16 = NeonF32Butterfly16::new(direction);
@@ -2852,6 +2852,11 @@ impl<T: FftNum> NeonF32Butterfly32<T> {
     }
 
     #[inline(always)]
+    pub fn new(direction: FftDirection) -> Self {
+        unsafe { NeonF32Butterfly32::neon_new(direction) }
+    }
+
+    #[inline(never)]
     unsafe fn perform_fft_contiguous(
         &self,
         input: RawSlice<Complex<f32>>,
@@ -2864,7 +2869,7 @@ impl<T: FftNum> NeonF32Butterfly32<T> {
         write_complex_to_array_strided!(out, output, 2, {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15});
     }
 
-    #[inline(always)]
+    #[inline(never)]
     pub(crate) unsafe fn perform_parallel_fft_contiguous(
         &self,
         input: RawSlice<Complex<f32>>,
