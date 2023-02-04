f7514edda62408311842a0570da5c94c3f657565
Sergey "Shnatsel" Davidoff <sdavydov@google.com>
2023-02-04 16:06:10 +0100

    Add basic fuzzing support for planned FFT, split by instruction set

---
+++ fuzz/fuzz_targets/scalar.rs
@@ -0,0 +1,19 @@
+#![no_main]
+
+use std::sync::Arc;
+
+use libfuzzer_sys::fuzz_target;
+
+use rustfft::num_complex::Complex;
+use rustfft::Fft;
+
+fuzz_target!(|data: Vec<f32>| {
+    let mut input = floats_to_complex(data);
+    let mut planner = rustfft::FftPlannerScalar::new();
+    let fft: Arc<dyn Fft<f32>> = planner.plan_fft_forward(input.len());
+    fft.process(&mut input);
+});
+
+fn floats_to_complex(floats: Vec<f32>) -> Vec<Complex<f32>> {
+    floats.chunks_exact(2).map(|pair| Complex { re: pair[0], im: pair[1] } ).collect()
+}
