b583fc7024f40558433df75e4a9a80645b7bd424
Elliott Mahler <join.together@gmail.com>
2026-09-21 18:29:48 -0700

    Revert "FftNd inherits from Fft instead of redefining all its methods"
    This reverts commit 4edf9cad5dc054acf3f69f90bfa735607ebf2aa4.

--- src/multidimensional/mod.rs
+++ src/multidimensional/mod.rs
@@ -79,7 +79,10 @@
 //! - A dimensionality of 1 is allowed and is exactly equivalent to computing a FFT with the non-multidimensional API.
 //! - A dimensionality of 0 is allowed and is a no-op and will always report its len() to be 0.
 
-use crate::{Fft, FftNum};
+use num_complex::Complex;
+use num_traits::Zero;
+
+use crate::{Direction, FftNum, Length};
 
 pub(crate) mod fft_nd_transpose;
 
@@ -93,7 +96,92 @@ pub(crate) mod multidimensional_test_utils;
 /// This trait has a few methods for computing FFTs. Its most conveinent method is [`process(slice)`](crate::FftNd::process).
 /// It takes in a slice of `Complex<T>` and computes a multidimensional FFT on that slice, in-place. It may copy the data over to internal scratch buffers
 /// if that speeds up the computation, but the output will always end up in the same slice as the input.
-pub trait FftNd<T: FftNum, const DIMENSIONS: usize>: Fft<T> {
+pub trait FftNd<T: FftNum, const DIMENSIONS: usize>: Length + Direction + Sync + Send {
+    /// Computes a multi-dimensional FFT in-place.
+    ///
+    /// Convenience method that allocates a `Vec` with the required scratch space and calls `self.process_with_scratch`.
+    /// If you want to re-use that allocation across multiple FFT computations, consider calling `process_with_scratch` instead.
+    ///
+    /// # Panics
+    ///
+    /// This method panics if:
+    /// - `buffer.len() % self.len_product() > 0`
+    /// - `buffer.len() < self.len_product()`
+    fn process(&self, buffer: &mut [Complex<T>]) {
+        let mut scratch = vec![Complex::zero(); self.get_inplace_scratch_len()];
+        self.process_with_scratch(buffer, &mut scratch);
+    }
+
+    /// Divides `buffer` into chunks of size `self.len()`, and computes a multi-dimensional FFT on each chunk.
+    ///
+    /// Uses the `scratch` buffer as scratch space, so the contents of `scratch` should be considered garbage
+    /// after calling.
+    ///
+    /// # Panics
+    ///
+    /// This method panics if:
+    /// - `buffer.len() % self.len() > 0`
+    /// - `buffer.len() < self.len()`
+    /// - `scratch.len() < self.get_inplace_scratch_len()`
+    fn process_with_scratch(&self, buffer: &mut [Complex<T>], scratch: &mut [Complex<T>]);
+
+    /// Divides `input` and `output` into chunks of size `self.len()`, and computes a multi-dimensional FFT on each chunk.
+    ///
+    /// This method uses both the `input` buffer and `scratch` buffer as scratch space, so the contents of both should be
+    /// considered garbage after calling.
+    ///
+    /// This is a more niche way of computing a FFT. It's useful to avoid a `copy_from_slice()` if you need the output
+    /// in a different buffer than the input for some reason. This happens frequently in RustFFT internals, but is probably
+    /// less common among RustFFT users.
+    ///
+    /// # Panics
+    ///
+    /// This method panics if:
+    /// - `output.len() != input.len()`
+    /// - `input.len() % self.len() > 0`
+    /// - `input.len() < self.len()`
+    /// - `scratch.len() < self.get_outofplace_scratch_len()`
+    fn process_outofplace_with_scratch(
+        &self,
+        input: &mut [Complex<T>],
+        output: &mut [Complex<T>],
+        scratch: &mut [Complex<T>],
+    );
+
+    /// Divides `input` and `output` into chunks of `self.len()`, and computes a multi-dimensional FFT on each chunk while
+    /// keeping `input` untouched.
+    ///
+    /// This method uses the `scratch` buffer as scratch space, so the contents should be considered garbage after calling.
+    ///
+    /// # Panics
+    ///
+    /// This method panics if:
+    /// - `output.len() ! input.len()`
+    /// - `input.len() % self.len() > 0`
+    /// - `input.len() < self.len()`
+    /// - `scratch.len() < get_immutable_scratch_len()`
+    fn process_immutable_with_scratch(
+        &self,
+        input: &[Complex<T>],
+        output: &mut [Complex<T>],
+        scratch: &mut [Complex<T>],
+    );
+
+    /// Returns the size of the scratch buffer required by `process_with_scratch`
+    ///
+    /// The returned value may change from one version of RustFFT to the next.
+    fn get_inplace_scratch_len(&self) -> usize;
+
+    /// Returns the size of the scratch buffer required by `process_outofplace_with_scratch`
+    ///
+    /// The returned value may change from one version of RustFFT to the next.
+    fn get_outofplace_scratch_len(&self) -> usize;
+
+    /// Returns the size of the scratch buffer required by `process_immutable_with_scratch`
+    ///
+    /// The returned value may change from one version of RustFFT to the next.
+    fn get_immutable_scratch_len(&self) -> usize;
+
     /// Returns FFT length of each dimension of this multidimensional FFT
     fn shape(&self) -> [usize; DIMENSIONS];
 }
