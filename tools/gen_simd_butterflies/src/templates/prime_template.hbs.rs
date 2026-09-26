48494300fd33a7450ce7881c5f41a4a7c8c08505
Elliott Mahler <join.together@gmail.com>
2026-09-26 01:49:18 -0700

    Switch butterfly target feature handling for sse and wasm simd

--- tools/gen_simd_butterflies/src/templates/prime_template.hbs.rs
+++ tools/gen_simd_butterflies/src/templates/prime_template.hbs.rs
@@ -9,6 +9,10 @@ use crate::array_utils::DoubleBuf;
 use crate::twiddles;
 use crate::{Direction, Fft, Length};
 
+use crate::fft_helper::{
+    fft_helper_immut, fft_helper_immut_unroll2x, fft_helper_inplace, fft_helper_inplace_unroll2x,
+};
+
 use super::{{arch.name_snakecase}}_common::{assert_f32, assert_f64};
 use super::{{arch.name_snakecase}}_utils::*;
 use super::{{arch.name_snakecase}}_vector::*;
